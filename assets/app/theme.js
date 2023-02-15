7b6f73c9645cb094066a515416b2d255edcef75f
natalia <noliv197@gmail.com>
2023-02-15 00:28:36 -0300

    upload starter pack | initial html / css layout and style

---
+++ assets/app/theme.js
@@ -0,0 +1,68 @@
+let style = {
+    theme1:{
+        background:{
+            main:"hsl(222, 26%, 31%)",
+            keypad:"hsl(223, 31%, 20%)",
+            screen:"hsl(224, 36%, 15%)"
+        },
+        colors:{
+            key1:"hsl(225, 21%, 49%)",
+            key2:"hsl(6, 63%, 50%)",
+            key3:"hsl(30, 25%, 89%)",
+        },
+        shadow:{
+            key1:"hsl(224, 28%, 35%)",
+            key2:"hsl(6, 70%, 34%)",
+            key3:"hsl(28, 16%, 65%)"
+        },
+        text:{
+            primary:"hsl(221, 14%, 31%)",
+            secondary:"hsl(0, 0%, 100%)"
+        }
+    },
+    theme2: {
+        background:{
+            main:"hsl(0, 0%, 90%)",
+            keypad:"hsl(0, 5%, 81%)",
+            screen:"hsl(0, 0%, 93%)"
+        },
+        colors:{
+            key1:"hsl(185, 42%, 37%)",
+            key2:"hsl(25, 98%, 40%)",
+            key3:"hsl(45, 7%, 89%)",
+        },
+        shadow:{
+            key1:"hsl(185, 58%, 25%)",
+            key2:"hsl(25, 99%, 27%)",
+            key3:"hsl(35, 11%, 61%)"
+        },
+        text:{
+            primary:"hsl(60, 10%, 19%)",
+            secondary:"hsl(0, 0%, 100%)",
+        }
+    },
+    theme3:{
+        background:{
+            main:"hsl(268, 75%, 9%)",
+            keypad:"hsl(268, 71%, 12%)",
+            screen:"hsl(268, 71%, 12%)"
+        },
+        colors:{
+            key1:"hsl(281, 89%, 26%)",
+            key2:"hsl(176, 100%, 44%)",
+            key3:"hsl(268, 47%, 21%)",
+        },
+        shadow:{
+            key1:"hsl(285, 91%, 52%)",
+            key2:"hsl(177, 92%, 70%)",
+            key3:"hsl(290, 70%, 36%)"
+        },
+        text:{
+            primary:"hsl(52, 100%, 62%)",
+            secondary:"hsl(198, 20%, 13%)",
+            third:"hsl(0, 0%, 100%)"
+        }
+    }
+}
+
+export default style
