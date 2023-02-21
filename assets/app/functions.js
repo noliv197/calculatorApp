d8d79888e95662b68d28081328d0e1823ebe5e08
natalia <noliv197@gmail.com>
2023-02-21 17:19:26 -0300

    operations adjustments

--- assets/app/functions.js
+++ assets/app/functions.js
@@ -4,37 +4,36 @@ export function resetCalculator(output,element){
 export function incrementCalculator(output,element){
     output.value = output.value + element.value
 }
-
 export function reset(output){
     output.value = '0'
 }
-
 export function del(output){
     output.value = output.value.slice(0,-1)
     if(output.value == '') output.value = '0'
 }
-
-export function sum(a,b){
-    return parseFloat(a) + parseFloat(b)
+export function findOperator(str){
+    let regex = /[^\d\.,]/
+    let result 
+    str[0] !== '-' ? result = str.match(regex) : result = str.substring(1).match(regex)
+    return (result !== null ? result[0] : result)
 }
-
-export function substraction(a,b){
-    return parseFloat(a) - parseFloat(b)
+export function checkEquation(str){
+    let result = splitEquation(str)
+    if(result.indexOf('') !== -1) result.splice(result.indexOf(''),1)
+    return result.length 
 }
-
-export function divide(a,b){
-    return b == 0 ? NaN : parseFloat(a) / parseFloat(b)
+export function checkFloat(str){
+    let result
+    let equation = splitEquation(str)
+    if(equation.length == 1 && equation[0].indexOf(',') == -1) result = true
+    else if(equation.length == 2 && equation[1].indexOf(',') == -1) result = true
+    else result = false
+    return result
 }
-
-export function multiple(a,b){
-    return parseFloat(a) * parseFloat(b)
-}
-
 export function calculate(output){
     output.value = convertToDot(output.value)
     let operator = findOperator(output.value)
     let [num1, num2] = getNumbers(output.value,operator)
-    console.log(num1,num2)
     let result
     switch (operator) {
         case '+': result =  sum(num1, num2)
@@ -54,6 +53,22 @@ export function calculate(output){
     output.value = result  
 }
 
+function sum(a,b){
+    return parseFloat(a) + parseFloat(b)
+}
+
+function substraction(a,b){
+    return parseFloat(a) - parseFloat(b)
+}
+
+function divide(a,b){
+    return b == 0 ? NaN : parseFloat(a) / parseFloat(b)
+}
+
+function multiple(a,b){
+    return parseFloat(a) * parseFloat(b)
+}
+
 function convertToDot(str){
     return (str.indexOf(',') !== -1 ? str.replace(',','.') : str)
 }
@@ -67,21 +82,10 @@ function getNumbers(str,operator){
         str.split(operator)
     )
 }
-export function findOperator(str){
-    let regex = /[^\d\.]/
-    let result
-    str[0] !== '-' ? result = str.match(regex) : result = str.substring(1).match(regex)
-    return (result !== null ? result[0] : result)
-}
-export function checkEquation(str){
+function splitEquation(str){
     let regex = /[^\d\.,]/
     let result  
     str[0] !== '-' ? result = str.split(regex) : result = str.substring(1).split(regex)
-    if(result.indexOf('') !== -1) result.splice(result.indexOf(''),1)
-    console.log(result)
-    return result.length 
+    return result
 }
 
-export function checkFloat(str){
-    
-}
--- assets/app/operations.js
+++ assets/app/operations.js
@@ -1,4 +1,4 @@
-import { resetCalculator, incrementCalculator, reset, del, calculate, findOperator, checkEquation } from "./functions.js"
+import { resetCalculator, incrementCalculator, reset, del, calculate, checkFloat, checkEquation } from "./functions.js"
 
 let output = document.querySelector('output')
 let resetButton = document.querySelector("[data-operator='clean']")
@@ -6,7 +6,7 @@ let delButton = document.querySelector("[data-operator='erase']")
 let calcButton = document.querySelector("[data-operator='calculate']")
 let chars = document.querySelectorAll('[data-char]')
 let charsArray = [...chars]
-let operators = [',','+','-','÷','x']
+let operators = ['+','-','÷','x']
 
 
 
@@ -16,19 +16,28 @@ calcButton.addEventListener('click',()=> calculate(output))
 
 charsArray.forEach(element => {
     element.addEventListener('click', ()=>{
+        let lastChar = output.value[output.value.length-1]
         // In case the result is NaN, the next click will reset the calculator
         if(output.value == 'NaN' ) reset(output)
         // If the calculator starts with 0 and the element is -, the calculator will display -
         else if(output.value == '0' && element.value == '-') resetCalculator(output,element)
+        // , exception
+        else if(output.value == '0' && element.value == ',') incrementCalculator(output,element)
         // If the calculator starts with 0 and the element is an operator, the calculator will display 0 plus the operator
         else if(output.value == '0' && operators.indexOf(element.value) == -1) resetCalculator(output,element)
+        // Exception ,
+        else if(element.value == ',') {
+            if(operators.indexOf(lastChar) !== -1) output.value = output.value + '0,'
+            else if(checkFloat(output.value)) incrementCalculator(output,element)
+        }
         // If the element is an operator, check to not repeat another operator subsequently
         else if(operators.indexOf(element.value) !== -1){
-            // Create exception for ,
             if(checkEquation(output.value) == 2) calculate(output)
-            else if(operators.indexOf(output.value[output.value.length-1]) == -1) incrementCalculator(output,element)
+            else if(lastChar == ',') output.value = output.value + '0' + element.value
+            else if(operators.indexOf(lastChar) == -1) incrementCalculator(output,element)
         }
         else incrementCalculator(output,element)
+
     })
 })
 
